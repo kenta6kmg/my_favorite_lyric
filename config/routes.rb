d82b4c9168f91193980b8ab72a3b8726d89d339d
kenta6kmg <atk_53.v@yahoo.ne.jp>
2021-06-03 10:37:50 +0900

    トップページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'lyrics/index'
+  root to: "lyrics#index"
+  resources :lyrics, only: :index
 end
