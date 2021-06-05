06aa2d7b0237a1738d81b35d882113456f4e96a0
kenta6kmg <atk_53.v@yahoo.ne.jp>
2021-06-05 22:15:34 +0900

    編集機能

--- app/controllers/lyrics_controller.rb
+++ app/controllers/lyrics_controller.rb
@@ -1,4 +1,6 @@
 class LyricsController < ApplicationController
+  before_action :move_to_index, except: :index
+  
   def index
     @lyrics = Lyric.all
   end
@@ -11,6 +13,15 @@ class LyricsController < ApplicationController
     Lyric.create(lyric_params)
   end
 
+  def edit
+    @lyric = Lyric.find(params[:id])
+  end
+
+  def update
+    lyric = Lyric.find(params[:id])
+    lyric.update(lyric_params)
+  end
+
   def destroy
     lyric = Lyric.find(params[:id])
     lyric.destroy
@@ -20,5 +31,10 @@ class LyricsController < ApplicationController
   def lyric_params
     params.require(:lyric).permit(:lyric, :song, :artist)
   end
-  
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'lyrics#index'
-  resources :lyrics, only: [:index, :new, :create, :destroy]
+  resources :lyrics, only: [:index, :new, :create, :destroy, :edit, :update]
 end
